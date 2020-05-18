bdf1b9ec11ebb583c649bf205e43f2c7509415e3
jovan996 <grgurjovan.jg@gmail.com>
2020-05-18 20:46:05 +0200

    small correction in model

--- Spring_boot_app/src/main/java/project/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-//Generated from repository.template file
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
-    
-}
